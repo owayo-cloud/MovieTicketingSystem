261a94f401e5cccda7b4f7ad7ac34264c56a93ed
owayo <owayopaul@gmail.com>
2024-12-27 01:25:00 +0300

    rename authentication app to user_auth and update configuration class

--- lukoba/user_auth/apps.py
+++ lukoba/user_auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationConfig(AppConfig):
+class UserAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name = 'user_auth'
