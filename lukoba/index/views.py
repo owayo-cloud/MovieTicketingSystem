55183807735e2d48319cb7c053aa132ee47a6cca
owayo <owayopaul@gmail.com>
2024-12-27 05:26:35 +0300

    add views for contacts and movies pages

--- lukoba/index/views.py
+++ lukoba/index/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
-	
+
+def contacts(request):
+    return render(request, 'contacts.html', {})
+
+def movies(request):
+    return render(request, 'movies.html', {})
+
+
