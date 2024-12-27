7236d87cf5d486e8ce82e18440b6bede6645f0f6
owayo <owayopaul@gmail.com>
2024-12-27 14:25:17 +0300

    add dashboard views for movies, bookings, and users

--- lukoba/index/views.py
+++ lukoba/index/views.py
@@ -10,4 +10,15 @@ def contacts(request):
 def movies(request):
     return render(request, 'movies.html', {})
 
+def dashboard(request):
+    return render(request, 'dashboard.html', {})
+
+def dashboardMovies(request):
+    return render(request, 'dashboard_movies.html', {})
+
+def dashboardBookings(request):
+    return render(request, 'dashboard_bookings.html', {})
+
+def dashboardUsers(request):
+    return render(request, 'dashboard_users.html', {})
 
