cdcb2c59a308e048668e89e79f3844ab62e30406
owayo <owayopaul@gmail.com>
2024-12-27 14:25:13 +0300

    add dashboard routes for movies, bookings, and users

--- lukoba/index/urls.py
+++ lukoba/index/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('movies', views.movies, name='movies'),
-    path('contacts', views.contacts, name='contacts'),
+    path('movies/', views.movies, name='movies'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/movies/', views.dashboardMovies, name='dashboard_movies'),
+    path('dashboard/bookings/', views.dashboardBookings, name='dashboard_bookings'),
+    path('dashboard/users/', views.dashboardUsers, name='dashboard_users'),
 ]
